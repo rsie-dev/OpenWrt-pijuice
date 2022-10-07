068332655d46db5dfdacef112e7a9eb6109350cd
sieger <sieger@zeus.swamp.de>
2022-10-07 10:57:42 +0200

    basic package working

---
+++ python3-pijuice/files/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import os
+
+version = os.environ.get('PIJUICE_VERSION')
+
+setup(
+    name="pijuice",
+    version=version,
+    author="Ton van Overbeek",
+    author_email="tvoverbeek@gmail.com",
+    description="Software package for PiJuice",
+    url="https://github.com/PiSupply/PiJuice/",
+    license='GPL v2',
+    py_modules=['pijuice'],
+    #data_files=[],
+    scripts=['src/pijuice_sys.py'],
+    )

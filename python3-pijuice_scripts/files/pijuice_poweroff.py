fe9d877f3967215637f3b13ca65fc526d30105c6
sieger <sieger@zeus.swamp.de>
2022-10-13 14:11:21 +0200

    added halt flag to prevent multiple calls

--- python3-pijuice_scripts/files/pijuice_poweroff.py
+++ python3-pijuice_scripts/files/pijuice_poweroff.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3 -OO
-
+import os
 import subprocess
 
 from pijuice import PiJuice
 
+HALT_FILE = '/tmp/pijuice_poweroff.flag'
+
 def systemHalt(pijuice):
     pijuice.status.SetLedBlink('D2', 3, [150, 0, 0], 200, [0, 100, 0], 200)
-    # Setting halt flag for 'pijuice_sys.py stop'
-    #with open(HALT_FILE, 'w') as f:
-    #    pass
+    # Setting halt flag 
+    with open(HALT_FILE, 'w') as f:
+        pass
     subprocess.call(["sudo", "halt"])
 
 def triggerPowerOff(pijuice):
@@ -16,9 +18,11 @@ def triggerPowerOff(pijuice):
     pijuice.power.SetPowerOff(20)
 
 def main():
-    pijuice = PiJuice(1, 0x14)
-    print("halt and completely power of")
+    if os.path.exists(HALT_FILE):
+        print("halt already triggered -> ignore")
 
+    print("halt and completely power of")
+    pijuice = PiJuice(1, 0x14)
     triggerPowerOff(pijuice)
     systemHalt(pijuice)
 

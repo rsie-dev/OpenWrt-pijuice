e2951053148012ee66f6d77bd9c12f2510bc3c96
sieger <sieger@zeus.swamp.de>
2022-10-14 17:40:48 +0200

    added support to enable wakeup on charge if configured

--- python3-pijuice_scripts/files/pijuice_poweroff.py
+++ python3-pijuice_scripts/files/pijuice_poweroff.py
@@ -4,10 +4,12 @@ import sys
 import logging
 import subprocess
 import argparse
+import json
 
 from pijuice import PiJuice
 
 HALT_FILE = '/tmp/pijuice_poweroff.flag'
+PiJuiceConfigDataPath = '/etc/pijuice/pijuice_config.JSON'
 
 def systemHalt(pijuice):
     pijuice.status.SetLedBlink('D2', 3, [150, 0, 0], 200, [0, 100, 0], 200)
@@ -24,10 +26,30 @@ def triggerPowerOff(pijuice, delay):
     if ret['error'] != 'NO_ERROR':
         raise IOError("Unable to set poweroff %s" % ret['error'])
 
+def loadPiJuiceConfig():
+    with open(PiJuiceConfigDataPath, 'r') as outputConfig:
+        pijuiceConfigData = json.load(outputConfig)
+        return pijuiceConfigData
+
+def enableWakeup(pijuice):
+    configData = loadPiJuiceConfig()
+    wakeupConfig = configData.get('system_task', {}).get('wakeup_on_charge', {})
+    enabled = wakeupConfig.get('enabled', False)
+    if not enabled:
+        logging.debug("wakeup not enabled")
+
+    trigger_level = wakeupConfig.get('trigger_level', 0)
+    if not trigger_level:
+        logging.debug("wakeup has no trigger level set")
+
+    logging.info("signal wakeup on charge at %s%%" % trigger_level)
+    pijuice.power.SetWakeUpOnCharge(trigger_level)
+
 def main():
     parser = argparse.ArgumentParser(description="halts and powers off")
     parser.add_argument('-v', '--verbose', action="store_true", help="verbose output")
     parser.add_argument('-d', '--delay', type=int, choices=range(10, 61), default=20, metavar="{10..60}", help="power off delay")
+    parser.add_argument('--noWakupEnable', action="store_true", help="do not enable wakup on charge if configured")
     args = parser.parse_args()
 
     if args.verbose:
@@ -42,9 +64,12 @@ def main():
             logging.warn("halt already triggered -> ignore")
             return 0
 
+        pijuice = PiJuice(1, 0x14)
+        if not args.noWakupEnable:
+            enableWakeup(pijuice)
+
         delay = args.delay
         logging.info("halt and completely power of after %ss" % delay)
-        pijuice = PiJuice(1, 0x14)
         triggerPowerOff(pijuice, delay)
     except: # pylint: disable=bare-except
         logging.exception("exception:")
